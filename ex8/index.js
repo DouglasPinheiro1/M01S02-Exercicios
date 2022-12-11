09efb6177b23fa1632a90121cecdba93014b73f6
DouglasPinheiro1 <douglas_pinheiro1@estudante.sesisenai.org.br>
2022-12-11 11:46:05 -0300

    Resolução Exercicio 08

---
+++ ex8/index.js
@@ -0,0 +1,17 @@
+//Ao entrar na página, seu script deve realizar 2 prompts ao usuário: um solicitando um “valor inicial”, e outro um valor para a “raíz”.
+//Após o usuário inserir esses valores, o seu script deve calcular os 10 primeiros valores da sequência e exibir na tela.
+//Relembrando: P.A. (Progressão Aritmética) é uma sequência numérica em que cada termo, a partir do segundo, é igual à soma do termo anterior com a raiz.
+//Exemplo: Valor inicial = 1; raiz = 3; P.A. = 1, 4, 7, 10, 13, 16, 19, 22, 25, 28.
+
+var numero = parseInt(prompt('Digite o número'));
+var raiz = parseInt(prompt('Digite a raiz'));
+
+var soma = numero;
+var resultado = numero.toString();
+console.log(numero, raiz);
+for (var i = 0; i < 9; i++) {
+  soma += raiz;
+  resultado += ', ' + soma;
+}
+
+alert('A PA é ' + resultado);
